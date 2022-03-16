18e32882b850a4c7f5a8ea6721137f667af644c1
Evan Anderson <40438869+TheeeEVan@users.noreply.github.com>
2022-03-16 18:53:22 +0000

    initialize react

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
 
-class App extends React.component {
-	return (
-		<main></main>
-	)
+class App extends React.Component {
+	constructor(props)
+	{
+		super(props)
+		this.state = {message: "this is my message"}
+	}
+	
+	render()
+	{
+		return(
+			<main><h1>Test</h1></main>
+		)
+	}	
 }
 
 export default App;
