9dda814267305428970727e5c44096079926e384
TheeeEVan <e.e.anderson@shaw.ca>
2022-05-13 15:26:23 -0600

    add base for building

--- scripts/setup.js
+++ scripts/setup.js
@@ -11,9 +11,9 @@ const fs = require('fs')
 
 // check for repl flag
 if (process.argv[2] == "repl") {
-	fs.writeFileSync("./vite.config.js", `import reactRefresh from"@vitejs/plugin-react-refresh";export default{plugins:[reactRefresh()],server:{host:"0.0.0.0",hmr:{port:443}}};`)
+	fs.writeFileSync("./vite.config.js", `import reactRefresh from"@vitejs/plugin-react-refresh";export default{plugins:[reactRefresh()],server:{host:"0.0.0.0",hmr:{port:443}},base:"/PokemonBattle/"};`)
 	console.log("\x1b[47m\x1b[30m%s\x1b[0m", "Vite config written for repl.it!")
 } else {
-	fs.writeFileSync("./vite.config.js", `import reactRefresh from"@vitejs/plugin-react-refresh";export default{plugins:[reactRefresh()],server:{port:3e3,host:"localhost",protocal:"ws"}};`)
+	fs.writeFileSync("./vite.config.js", `import reactRefresh from"@vitejs/plugin-react-refresh";export default{plugins:[reactRefresh()],server:{port:3e3,host:"localhost",protocal:"ws"},base:"/PokemonBattle/"};`)
 	console.log("\x1b[47m\x1b[30m%s\x1b[0m", "Vite config written for local machine!")
 }
